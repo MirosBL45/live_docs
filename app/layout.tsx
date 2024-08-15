87a8e32051963b790c40556b1ea03e1121cdeb28
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-08-15 23:25:08 +0200

    clerk added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,45 @@
-import "./globals.css"
-import { Inter as FontSans } from "next/font/google"
+import './globals.css';
+import { Inter as FontSans } from 'next/font/google';
 
-import { cn } from "@/lib/utils"
-import { Metadata } from "next"
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+
+import { cn } from '@/lib/utils';
+import { Metadata } from 'next';
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
-  title: "LiveDocs",
-  description: "Your go-to collaborative editor with Next.js",
-}
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor with Next.js',
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      {/* <head /> */}
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: '#3371FF', fontSize: '16px' },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        {/* <head /> */}
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased',
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header({ children }: HeaderProps) {
+  return (
+    <header className="header">
+      <Link href={'/'} className="md:flex-1">
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+      {children}
+    </header>
+  );
+}
